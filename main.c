289040f65423cef8b00b92e70b5bf93c1e82d847
gehad-aboelmagd <jhad44734@gmail.com>
2025-10-24 12:05:10 +0300

    adding add function

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int sub(int x, int y)
+{
+	return x - y;
+}
+
+// first
+// second
+// third
+
 int main()
 {
 	printf("Hello, world\n");
